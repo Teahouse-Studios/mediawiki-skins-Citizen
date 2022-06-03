ae6e612f44f9772168fbe0caeda73001af4e8396
Dianliang233 <dianliang233@gmail.com>
2022-06-03 13:53:21 +0800

    Use new footer

--- resources/skins.citizen.scripts/skin.js
+++ resources/skins.citizen.scripts/skin.js
@@ -115,6 +115,8 @@ function main( window ) {
 	}
 
 	mw.loader.load( 'skins.citizen.preferences' );
+	mw.loader.load( 'https://story-cdn.teahouse.team/t-footer/assets/index.js' );
+	mw.loader.load( 'https://story-cdn.teahouse.team/t-footer/assets/index.css', 'text/css' )
 }
 
 main( window );
